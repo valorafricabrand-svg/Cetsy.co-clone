971b220c76e52884aeb4abb3e2a14f90d0b0ad48
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-08-14 05:00:09 +0300

    first commit

--- cetsy_app/lib/config/constants.dart
+++ cetsy_app/lib/config/constants.dart
@@ -1,7 +1,7 @@
 /// Global constants used throughout the app
 class Constants {
   // Base URL for Laravel API (adjust if you're on a real device or hosting)
-  static const String baseUrl = "https://cetsy2.ratemata.com/api"; // Use 10.0.2.2 for Android emulator
+  static const String baseUrl = "https://cetsy.co/api"; // Use 10.0.2.2 for Android emulator
 
   // If you're testing on a real device connected to same network:
   // static const String baseUrl = "http://192.168.1.100:8000/api"; // Replace with your IP
