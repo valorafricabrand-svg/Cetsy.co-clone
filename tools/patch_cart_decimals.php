4da03c43f2d93b5e6c76269b061a1c9801ae01c3
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-09-15 00:41:23 +0300

    fix issue  commit

---
+++ tools/patch_cart_decimals.php
@@ -0,0 +1,6 @@
+﻿<?php
+$p = 'resources/views/theme/cetsy/partials/_cart.blade.php';
+$s = file_get_contents($p);
+$s = preg_replace('/data-decimals="[^"]*"/', 'data-decimals="{{ $__dec }}"', $s);
+file_put_contents($p, $s);
+?>
