93cd750eadcaf6e7d4fc77b7c56da7c62a3326ba
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-10-08 02:22:27 +0300

    Stage remaining changes

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -271,7 +271,7 @@
             </div>
             <div class="col-12 d-flex justify-content-between">
               <span class="text-muted">Country</span>
-              <span class="fw-medium">{{ $shop->country }}</span>
+              <span class="fw-medium">{{ country_name($shop->country) }}</span>
             </div>
             <div class="col-12 d-flex justify-content-between">
               <span class="text-muted">Currency</span>
