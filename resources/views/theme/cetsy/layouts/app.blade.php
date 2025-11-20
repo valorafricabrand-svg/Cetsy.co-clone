fe2f67c9f27b6993ee80a5bdca4cf627017cf466
Ezekiel Muki <ezekielmuki42@gmail.com>
2025-11-20 15:21:46 +0300

    migration fixingt commit

--- resources/views/theme/cetsy/layouts/app.blade.php
+++ resources/views/theme/cetsy/layouts/app.blade.php
@@ -1151,7 +1151,9 @@
             <h4 class="text-uppercase mb-3 footer-heading text-white">Help &amp; Support</h4>
             <ul class="list-unstyled mb-0 footer-text">
               <li class="mb-2"><a href="{{ url('/user-agreement#house-rules') }}" class="footer-link text-white-50 text-decoration-none">Help &amp; FAQs</a></li>
-              <li class="mb-2"><a href="{{ url('/contact') }}" class="footer-link text-white-50 text-decoration-none">Contact us</a></li>
+              <li class="mb-2">
+                <a href="https://docs.google.com/forms/d/1uOyZN2ysRSZBOpYneYQKwlZdish465UMxfil8cEv5vE/viewform?edit_requested=true" class="footer-link text-white-50 text-decoration-none" target="_blank" rel="noopener">Contact us</a>
+              </li>
               <li class="mb-2"><a href="{{ url('/user-agreement#terms') }}" class="footer-link text-white-50 text-decoration-none">Terms &amp; conditions</a></li>
               <li class="mb-2"><a href="{{ url('/user-agreement#privacy') }}" class="footer-link text-white-50 text-decoration-none">Privacy policy</a></li>
             </ul>
